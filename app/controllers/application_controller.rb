bda6a5e8add5826c76a6f32c0a3858ebe6764fea
Chris Powell <chris.ian.powell@gmail.com>
2018-12-10 11:10:18 -0700

    Adds complete es.yml for translations and locale param links to the root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :change_locale
   helper_method :current_user
-  
+
+private
+
   def current_user
     @current_user ||= User.find_by_uid(session[:user_id]) if session[:user_id]
+  end
+
+  def change_locale
+    I18n.locale = params[:locale] if params[:locale].present?
   end
 end
