e84451eeca6af4bbb030cee2deaf1e44dedba190
Chris Powell <chris.ian.powell@gmail.com>
2018-12-10 10:39:29 -0700

    Adds functionality for all views to read en.yml through I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   root to: 'dashboard#index'
-  get '/locations', to: 'location#new'
+
   get '/zip_location', to: 'zip#index'
   get '/state_location', to: 'state#index'
   get '/county_birds', to: 'county#index'
-  get '/bird_map', to: 'map#show'
+  get '/users/:id', to: 'users#show', as: 'user'
   get 'auth/google_oauth2', as: 'new_user'
   get '/auth/google_oauth2/callback', to: 'session#create'
+  
   post '/users/:id/favorites', to: 'users#create', as: 'favorites'
-  resources :users, only: [:show]
+
   delete '/users', to: 'users#destroy'
   delete '/session', to: 'session#destroy'
 end
