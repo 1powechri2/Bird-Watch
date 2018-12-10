548d1c60d1a27bef938908cb2bdb1c4f30c4578c
Chris Powell <chris.ian.powell@gmail.com>
2018-12-10 11:46:22 -0700

    Adds translation to espanol for every feature except pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ private
   def change_locale
     I18n.locale = params[:locale] if params[:locale].present?
   end
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  root to: 'dashboard#index'
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    root to: 'dashboard#index'
 
-  get '/zip_location', to: 'zip#index'
-  get '/state_location', to: 'state#index'
-  get '/county_birds', to: 'county#index'
-  get '/users/:id', to: 'users#show', as: 'user'
+    get '/zip_location', to: 'zip#index'
+    get '/state_location', to: 'state#index'
+    get '/county_birds', to: 'county#index'
+    get '/users/:id', to: 'users#show', as: 'user'
+
+    post '/users/:id/favorites', to: 'users#create', as: 'favorites'
+
+    delete '/users', to: 'users#destroy'
+    delete '/session', to: 'session#destroy'
+  end
   get 'auth/google_oauth2', as: 'new_user'
   get '/auth/google_oauth2/callback', to: 'session#create'
-  
-  post '/users/:id/favorites', to: 'users#create', as: 'favorites'
-
-  delete '/users', to: 'users#destroy'
-  delete '/session', to: 'session#destroy'
 end
